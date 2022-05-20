67f85c17abdaca844dcb4cb5491485525905fe6f
ChrisMwanya <cmwanya@gmail.com>
2022-05-20 14:14:23 +0100

    feat: add route studentuser

--- api/profil/controllers/profil.js
+++ api/profil/controllers/profil.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+
+const { default: createStrapi } = require("strapi");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async getStudent(ctx) {
+    const userType = await strapi
+      .query("User-type")
+      .findOne({ type: "Student" });
+
+    const studentReponse = await strapi
+      .query("user", "users-permissions")
+      .model.find({ user_type: userType.id });
+
+    return studentReponse;
+  },
+};
