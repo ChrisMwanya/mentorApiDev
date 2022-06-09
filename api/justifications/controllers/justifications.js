25eed614177d8dd308aaef8f2e106cf7d78466b5
Sacre-MBIKU <sajombiku@gmail.com>
2022-06-09 16:59:18 +0200

    feat: create justification details collection

--- api/justifications/controllers/justifications.js
+++ api/justifications/controllers/justifications.js
@@ -10,8 +10,9 @@ const { sanitizeEntity } = require("strapi-utils");
 module.exports = {
   async findOneByUser(ctx) {
     const { id } = ctx.params;
-    const entity = await strapi.query("justifications").model.find({user: id});
-
+    const entity = await strapi
+      .query("justifications")
+      .model.find({ user: id });
     return sanitizeEntity(entity, { model: strapi.models.attendance });
   },
 };
